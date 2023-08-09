51d8f849b6dcf2c9475831b9b06f80414959141f
Amr Saeed <amrsaied47@gmail.com>
2023-08-09 22:35:37 +0300

    edited start.sh file

--- start.sh
+++ start.sh
@@ -1,17 +1,27 @@
 set -e
 
-cp /src/.env.example /src/.env
+echo "=== Copy .env  ==="
+cp ./src/.env.example ./src/.env
+echo "=== copy is done!  ==="
 
-docker-compose run --rm artisan key:generate
+echo "=== Execute docker-compose build... ==="
+docker-compose up --build -d
+echo "=== docker-compose build is done!! ==="
 
-docker-compose up -d --build
-
+echo "=== Installing composer... ==="
 docker-compose run --rm composer install
+echo "=== Composer build is done ==="
 
+echo "==== Generating the app key ==="
+docker-compose run --rm artisan key:generate
+echo "==== App key generating is done ==="
+
+echo "==== Installing npm==="
 docker-compose run --rm npm install
-
 docker-compose run --rm npm run dev
+echo "==== npm installed ==="
 
+echo "==== Migrating and seed database ==="
 docker-compose run --rm artisan migrate:fresh --seed
-
 docker-compose run --rm php vendor/bin/phpunit
+echo "==== Migrating and seed database is done ==="
